356bf093a63a0e984af17488f9e0c756e9bc0c02
Emiliano <140673149+Emikologcito@users.noreply.github.com>
2025-11-20 16:27:50 -0300

    Añadir archivo "Asientos.py" con la clase Asientos
    Creamos la clase después así que la clase Asientos no existía, aquí esta.

---
+++ Asientos.py
@@ -0,0 +1,13 @@
+class Asientos:
+
+    def __init__(self, Fila:int, Columna:int, Disponible:bool, Sala):
+        self.Fila = Fila
+        self.Columna = Columna
+        self.Disponible = Disponible
+        self.Sala = Sala
+
+#   def Asientos(self):
+#       Muestra todos los asientos
+
+#   def MarcarOcupado(self):
+#       return void
